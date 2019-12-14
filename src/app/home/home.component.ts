fc1b97faf7ea8a8b94c505c6e7cb045cb4c4ca5f
aminovishe <Mayno0000>
2019-12-14 21:54:26 +0100

    buy product front-office

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,9 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {Utils} from '../shared/utils';
 import {ProductService} from '../shared/services/product.service';
 import {Product} from '../shared/models/product';
+import {NgForm} from '@angular/forms';
+import {AuthGuard} from '../auth.guard';
+import Swal from 'sweetalert2';
 declare var $: any;
 
 @Component({
@@ -12,14 +15,30 @@ declare var $: any;
 })
 export class HomeComponent implements OnInit {
   products: Product[];
+  isLoggedIn = !this.authGuard.canActivate();
 
   ngOnInit() {
   }
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService,
+              private authGuard: AuthGuard) {
     this.productService.listProducts().subscribe((products: Product[]) => {
       this.products = products;
       Utils.initializeSliders(300);
+    });
+  }
+
+  buyProd(form: NgForm) {
+    const quantity = form.controls.quantity.value;
+    const productId = form.controls.id.value;
+    const token = localStorage.getItem('token');
+    this.productService.buyProduct(productId, quantity, token).subscribe(response => {
+      if (response['success']) {
+        Swal.fire({icon: 'success', title: 'Merci !!', text: response['success']});
+      } else {
+        Swal.fire({icon: 'error', title: 'Erreur !!', text: response['error']});
+      }
+
     });
   }
 
--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -15,12 +15,28 @@ export class ProductService {
 
   private readonly apiUrl = Config.baseUrl;
   private productUrl = this.apiUrl + '/product';
+  private buyUrl = this.apiUrl + '/buy';
   private products;
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    })
+  };
 
   constructor(private httpClient: HttpClient) { }
 
   /** GET Products */
   listProducts (): Observable<Product[]> {
     return this.httpClient.get<Product[]>(this.productUrl);
+  }
+
+  /** Buy Product */
+  buyProduct (productId, quantity, token) {
+    return this.httpClient.post(this.buyUrl, {
+      'productId': productId,
+      'quantity': quantity,
+      'token': token
+    });
   }
 }
